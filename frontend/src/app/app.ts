8f29b46427d36e78ebe9a138b01e08c835b0856b
Fabian Feichter <me@fabian-feichter.at>
2026-06-16 21:03:28 +0200

    Add loading screen prior to Keycloak login redirect

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,6 +1,9 @@
-import {ChangeDetectionStrategy, Component, signal} from '@angular/core';
+import {ChangeDetectionStrategy, Component, inject} from '@angular/core';
 import {ClrBreadcrumbsModule, ClrDropdownModule, ClrIconModule, ClrVerticalNavModule,} from '@clr/angular';
 import {Layout} from './layout/layout';
+import {OidcSecurityService} from 'angular-auth-oidc-client';
+import {toSignal} from '@angular/core/rxjs-interop';
+import {map} from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -10,5 +13,11 @@ import {Layout} from './layout/layout';
   styleUrl: './app.css',
 })
 export class App {
-  protected readonly title = signal('reparierenstattwegwerfen-backoffice-frontend');
+
+  private readonly oidc = inject(OidcSecurityService);
+
+  readonly isAuthenticated = toSignal(
+    this.oidc.isAuthenticated$.pipe(map(({isAuthenticated}) => isAuthenticated)),
+    {initialValue: false}
+  );
 }
