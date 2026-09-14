615252a979a991db512b937a35f3392e78181fea
Fabian Feichter <me@fabian-feichter.at>
2026-09-15 00:50:53 +0200

    Added prototype for scrollspy in device page

--- frontend/src/app/domain/device/device-detail/device-detail.ts
+++ frontend/src/app/domain/device/device-detail/device-detail.ts
@@ -4,11 +4,14 @@ import {
   ClrCommonFormsModule,
   ClrDatagridModule,
   ClrInputModule,
+  ClrMainContainerModule,
   ClrModalModule,
   ClrNumberInputModule,
   ClrRadioModule,
   ClrSpinnerModule,
+  ClrTabsModule,
   ClrTextareaModule,
+  ClrVerticalNavModule,
 } from '@clr/angular';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { Notes } from './notes/notes';
@@ -17,6 +20,7 @@ import { Repair } from './repair/repair';
 import { Activity } from './activity/activity';
 import { Selling } from './selling/selling';
 import { Buying } from './buying/buying';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-device-detail',
@@ -37,6 +41,10 @@ import { Buying } from './buying/buying';
     Activity,
     Selling,
     Buying,
+    RouterLink,
+    ClrVerticalNavModule,
+    ClrTabsModule,
+    ClrMainContainerModule,
   ],
   templateUrl: './device-detail.html',
   standalone: true,
@@ -47,12 +55,23 @@ export class DeviceDetail implements OnInit {
     transform: (value: string) => Number(value),
   });
   deviceBase = signal<DeviceBaseDetailsDto | undefined>(undefined);
-
+  navItems = [
+    { id: 'device', label: 'Device' },
+    { id: 'buying', label: 'Buying' },
+    { id: 'repair', label: 'Repair' },
+    { id: 'selling', label: 'Selling' },
+    { id: 'notes', label: 'Notes' },
+    { id: 'activity', label: 'Activity' },
+  ];
   private baseApi = inject(DeviceBaseControllerService);
 
   ngOnInit(): void {
     this.baseApi
       .getDeviceBaseDetails(this.deviceId())
       .subscribe((data) => this.deviceBase.set(data));
+  }
+
+  scrollToSection(sectionId: string): void {
+    document.getElementById(sectionId)?.scrollIntoView({ behavior: 'smooth' });
   }
 }
